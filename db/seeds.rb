c6655f0c2f7be8e4d560215e0ba75ac9fe2ccf31
Hoi Yan Lam <iamhy10421@gmail.com>
2022-02-02 16:50:53 +0100

    restarant challenge with rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+epicure = Restaurant.new(name: 'Epicure', address: 'Rue du 22 Septembre', category: 'french')
+epicure.save
+best_pizza = Restaurant.new(name: 'Best Pizza', address: 'Rue du 12 hahaha', category: 'chinese')
+best_pizza.save
+what_a_dish = Restaurant.new(name: 'What a dish', address: 'Rue du 33 Octobre', category: 'italian')
+what_a_dish.save
+mommy_bakery = Restaurant.new(name: 'Mommy Bakery', address: 'Rue du 44 Decembre', category: 'japanese')
+mommy_bakery.save
+comptoir_de_soir = Restaurant.new(name: 'Comptoir de soir', address: 'Rue du 7 August', category: 'belgian')
+comptoir_de_soir.save
